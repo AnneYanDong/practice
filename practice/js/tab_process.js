5742d5b64d7b6bcc67ab8b8c33b18801b0d67341
yandong <yandong@jianbing.com>
2017-06-22 16:50:03 +0800

    面向过程的tab切换的js文件

---
+++ practice/js/tab_process.js
@@ -0,0 +1,24 @@
+function getId(id) {
+	return typeof id === 'string' ? document.getElementById(id) : id;
+}
+window.onload = function () {
+	var titLis = getId('notice-title').getElementsByTagName('li');
+	var mods = getId('notice-content').getElementsByTagName('div');
+	console.log(mods.length + titLis.length);
+	//确保titles的个数与content个数相等
+	if (titLis.length != mods.length) return;
+	for(var i = 0;i < titLis.length; i ++) {
+		console.log(titLis[i]);
+		//给每个li加上索引属性id，方便后面的content里面的div调用
+		titLis[i].id = i;
+		//鼠标滑过时给每一个li元素加上select高亮显示
+		titLis[i].onmouseover = function() {
+			for(var j = 0;j < titLis.length;j ++) {
+				titLis[j].className = '';//添加高亮之前去除之前的高亮样式，不然就会点一个高亮增加一个，而不是只选中一个
+				mods[j].style.display = 'none'; //全部隐藏，方便后边mods显示内容
+			}
+			this.className = 'select';
+			mods[this.id].style.display = 'block';
+		}
+	}
+}
